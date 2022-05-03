cf7984dd958bc6a70642cb953bf4002a66672a07
Mirhanda <mhopealewine@gmail.com>
2022-05-03 18:44:08 -0400

    initial commit

--- shoppingCartAdd.jsx
+++ shoppingCartAdd.jsx
@@ -6,19 +6,23 @@
 function NavBar({ stockitems }) {
   const [cart, setCart] = React.useState([]);
   const [stock, setStock] = React.useState(stockitems);
-  const { Button } = ReactBootstrap;
+  const { Card, Button } = ReactBootstrap;
   // event apple:2
   const moveToCart = e => {
     let [name, num] = e.target.innerHTML.split(":"); // innerHTML should be format name:3
     // use newStock = stock.map to find "name" and decrease number in stock by 1
     // only if instock is >=  do we move item to Cart and update stock
+    if (num <=0) return;
+    // get item with name from stock and update stock
+    let item = stock.filter((item) => item.name == name);
     let newStock = stock.map((item, index) => {
       if (item.name == name) item.instock--;
       return item;
     });
-    setStock(newStock);
+    setStock([...newStock]);
+    setCart([...cart, ...item]);
   };
-  const updatedList = stock.map((item, index) => {
+  const updatedList = menuitems.map((item, index) => {
     return (
       <Button onClick={moveToCart} key={index}>
         {item.name}:{item.instock}
@@ -30,9 +34,24 @@ function NavBar({ stockitems }) {
     <>
       <ul style={{ listStyleType: "none" }}>{updatedList}</ul>
       <h1>Shopping Cart</h1>
+      <Cart cartitems={cart}>Cart Items</Cart>
     </>
   );
 }
+
+function Cart({ cartitems }) {
+  const { Card, Button } = ReactBootstrap;
+  console.log("rendering Cart");
+  const updatedList = cartitems.map((item, index) => {
+    return <Button key={index}>{item.name}</Button>;
+  });
+  return (
+    <ul style={{ listStyleType: "none" }} key="cart">
+      {updatedList}
+    </ul>
+  );
+}
+
 const menuItems = [
   { name: "apple", instock: 2 },
   { name: "pineapple", instock: 3 },
